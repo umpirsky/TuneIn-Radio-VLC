0a957f533eb4db4ace2826d04c6b7367fcb7741f
umpirsky <umpirsky@gmail.com>
2014-10-08 00:04:04 +0200

    Fix station title and artist

--- tunein.lua
+++ tunein.lua
@@ -31,12 +31,12 @@ end
 function main()
 
 	-- Add the main categories to browse TuneIn
-	
+
 	-- Check is the username was defined
 	-- WISH: Dialog to change this params because vlc.config object doesn't work
 	local __username__ = "user"
 	local __password__ = "password"
-	
+
 	tunein_radio = NewTuneInRadio (__username__, __password__)
 
 	--
@@ -49,9 +49,9 @@ function main()
 	-- track_type is category, region, id
 	--
 	if __username__ ~= nil then
-		tunein_radio.add_radio_tracks ( "category", "presets", "My presets" )
+		tunein_radio.add_radio_tracks ( "category", "presets", "My library" )
 	end
-	
+
 	tunein_radio.add_radio_tracks ( "category", "local", "Local Radio")
 	tunein_radio.add_radio_tracks ( "category", "trending", "Trending")
 	tunein_radio.add_radio_tracks ( "category", "music", "Music")
@@ -78,12 +78,12 @@ function NewTuneInRadio (username, password)
 					__BASE_URL__ = "opml.radiotime.com",
 					__formats__ = "aac,html,mp3,wma,wmpro,wmvideo,wmvoice"
 	}
-    
+
 	--
 	-- Load the genre array in a cache
 	--
 	local load_genre_cache = function ()
-			
+
 		-- Local Variables
 		local params = ""
 		local method = "/Describe.ashx"
@@ -118,16 +118,16 @@ function NewTuneInRadio (username, password)
 			end
 		end
 	end
-    
+
 	--
 	-- Add Radio Tracks Functions
 	--
 	local add_radio_tracks = function ( track_type, category, category_name, username, password )
-		
+
 		-- Local Variables
 		local params = ""
 		local method = "/Browse.ashx"
-	
+
 		-- Create the params string using track_type
 		if track_type == "category" then
 			params = "?c=" .. category
@@ -137,38 +137,36 @@ function NewTuneInRadio (username, password)
 			params = "?id=" .. category
 		end
 		params = params .. "&formats=" .. self.__formats__ .. "&partnerId=" .. self.__partner_id__ .. "&username=" .. self.__username__ .. "&password=" .. self.__password__
-	
+
 		-- Create the URL
 		local url = self.__protocol__ .. self.__BASE_URL__ .. method .. params
-	
+
 		-- Add the first node
-		local node = vlc.sd.add_node( {	title = category_name,
-									 	arturl = "http://www.avrilcolombia.com/VideoLAN/VLC/resources/" .. category .. ".png"
-									 } )
+		local node = vlc.sd.add_node( {	title = category_name } )
 		local tree = simplexml.parse_url(url)
-			
+
 		for _, body in ipairs( tree.children ) do
 			simplexml.add_name_maps( body )
-			
+
 			-- This has found an station
 			if body.children_map["status"] == nil then
 				if body.children_map["outline"] ~= nil then
-					
+
 					-- Browse all outline elements searching stations
 					for _, station in ipairs( body.children_map["outline"] ) do
 						if station ~= nil then
-							
+
 							-- Add Station
 							-- Check if the station is a Radio Station
 							if station.attributes["type"] == "audio" then
 								-- Its a station
 								node:add_subitem( {path = station.attributes["URL"],
-												title = station.attributes["subtext"],
-												artist = station.attributes["text"],
+												title = station.attributes["text"],
+												artist = station.attributes["subtext"],
 												genre = get_genre_name ( station.attributes["genre_id"] ),
 												arturl = vlc.strings.resolve_xml_special_chars ( station.attributes["image"] )
 								} )
-							
+
 							elseif station.attributes["type"] == "link" then
 								-- Its a Subnode (Link)
 								node:add_subitem( {path = station.attributes["URL"],
@@ -176,7 +174,7 @@ function NewTuneInRadio (username, password)
 												artist = station.attributes["text"],
 												album = station.attributes["text"],
 								} )
-							
+
 							else
 								-- Its a Subnode only
 								-- WISH. Can display the entire tree
